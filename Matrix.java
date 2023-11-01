1a5637bc30f66de995f036f36222707ccb1261cd
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-11-01 15:55:20 -0400

    adding a CLI to the program

--- Matrix.java
+++ Matrix.java
@@ -23,7 +23,7 @@ public class Matrix {
     }
   }
 
-  public void populatMatrix() {
+  public void populateMatrix() {
     for(int i = 0; i < size; i++) {
       for(int j = 0; j < size; j++) {
         matrix[i][j] = (i * size) + j + 1;
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,16 +1,39 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    Matrix h = new Matrix(8);
 
-    System.out.println();
+    int size;
+    Scanner scan  = new Scanner(System.in);
+
+    System.out.print("\tPlease enter the size of your matrix: ");
+
+    try {
+      size = scan.nextInt();
+      scan.close();
+    } catch (Exception e) {
+      scan.close();
+      throw new InputMismatchException("Input must be an int");
+    }
+
+    System.out.printf("\n\tYour matrix is %d x %d\n", size, size);
+
+    Matrix h = new Matrix(size);
+
+    System.out.println("\n\tPrinting matrix with default values:");
     h.printMatrix();
-    System.out.println();
-    h.populatMatrix();
+
+    System.out.println("\n\tPopulating matrix...matrix populated");
+    h.populateMatrix();
+
+    System.out.println("\n\tPrinting matrix:");
     h.printMatrix();
-    System.out.println();
+
+    System.out.println("\n\tFlipping matrix...matrix flipped");
     h.flipMatrix();
+
+    System.out.println("\n\tPrinting flipped matrix:");
     h.printMatrix();
   }
 }
